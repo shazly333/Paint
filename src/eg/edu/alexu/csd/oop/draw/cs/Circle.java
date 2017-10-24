5e963078f3e4befde4808a49196bea76b7c574c2
Ayman <ayman.a.samy.m@gmail.com>
2017-10-24 14:21:26 +0200

    Init commit

---
+++ src/eg/edu/alexu/csd/oop/draw/cs/Circle.java
@@ -0,0 +1,21 @@
+package eg.edu.alexu.csd.oop.draw.cs;
+
+import java.awt.*;
+
+public class Circle extends TheShape {
+
+    public Circle()
+    {
+
+    }
+
+    @Override
+    public void draw(Graphics canvas) {
+
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return null;
+    }
+}
